0b3cd19fe784112987b1e2d9ea6ec8ed3019750b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 11:14:48 +0200

    Render checkboxes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+import { checkboxes } from "./checkboxes";
+import { Checkbox } from "./components/Checkbox";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(checkboxes);
+  }, []);
   return (
     <div className="App">
-     
+      {checkboxes.map(checkbox => <Checkbox key={checkbox.id} checkbox={checkbox}/>)}
     </div>
   );
 }
