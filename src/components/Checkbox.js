dfe8d8ec22728c9e01026f957884cafbddb7e41e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 11:21:31 +0200

    Add input properties

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,9 +1,16 @@
-export const Checkbox = ({checkbox}) => {
+import CHECKICON from '../images/ic_checkbox_checked.svg';
+import UNCHECKICON from '../images/ic_checkbox_unchecked.svg';
+
+export const Checkbox = ({ checkbox, onEdit }) => {
     return (
         <div className="checkboxes">
             <label>
-                <img src="" alt="" />
-                <input type="checkbox" />
+                <img className="checkbox__img" src={checkbox.checked ? CHECKICON : UNCHECKICON} alt="" />
+                <input type="checkbox"
+                    onChange={() => !checkbox.checked}
+                    checked={checkbox.checked}
+                    onClick={() => onEdit(checkbox.id)}
+                    style={{ display: "none" }} />
                 {checkbox.label}
             </label>
         </div>
