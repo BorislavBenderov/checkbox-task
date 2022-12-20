b94b17e86fbacbbc8b53a5037f89b56efdd05b72
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 11:07:38 +0200

    Add checkboxes array of objects

---
+++ src/checkboxes.js
@@ -0,0 +1,27 @@
+export const checkboxes = [
+    {
+        id: 1,
+        label: 'Text 1',
+        checked: false
+    },
+    {
+        id: 2,
+        label: 'Text 2',
+        checked: false
+    },
+    {
+        id: 3,
+        label: 'Text 3',
+        checked: false
+    },
+    {
+        id: 4,
+        label: 'Text 4',
+        checked: false
+    },
+    {
+        id: 5,
+        label: 'Text 5',
+        checked: false
+    },
+]
